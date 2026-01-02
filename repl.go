e0927defa0415d8c8fec98726e7af9bff1da606e
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-01-01 19:40:05 -0500

    Added groundwork for cleanInput() and associated tests

---
+++ repl.go
@@ -0,0 +1,9 @@
+package main
+
+// The purpose of this function will be to split the user's input into "words" 
+// based on whitespace. It should also lowercase the input and trim any leading
+// or trailing whitespace.
+func cleanInput(text string) []string {
+	// TODO: implement
+    return []string{}
+}
