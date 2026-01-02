0ae96c3a551b7eb5e89a34dbbdece460176d77b6
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-01-01 20:44:09 -0500

    Added cleanInput() with some test cases

--- repl.go
+++ repl.go
@@ -1,9 +1,15 @@
 package main
 
-// The purpose of this function will be to split the user's input into "words" 
+import (
+	"strings"
+)
+
+// The purpose of this function will be to split the user's input into "words"
 // based on whitespace. It should also lowercase the input and trim any leading
 // or trailing whitespace.
 func cleanInput(text string) []string {
-	// TODO: implement
-    return []string{}
+
+	retString := strings.Fields(strings.ToLower(text))
+
+    return retString
 }
--- repl_test.go
+++ repl_test.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-    "testing"
+	"testing"
+    "fmt"
 )
 
 func TestCleanInput(t *testing.T) {
@@ -14,6 +15,11 @@ func TestCleanInput(t *testing.T) {
             expected: []string{"hello", "world"},
         },
         // add more cases here
+        {
+            // Charmander Bulbasaur PIKACHU -> ["charmander", "bulbasaur", "pikachu"]
+            input:  "Charmander Bulbasaur PIKACHU",
+            expected: []string{"charmander", "bulbasaur", "pikachu"},
+        },
     }
 
     for _, c := range cases {
@@ -21,12 +27,22 @@ func TestCleanInput(t *testing.T) {
         // Check the length of the actual slice against the expected slice
         // if they don't match, use t.Errorf to print an error message
         // and fail the test
+        fmt.Printf("Actual:   %v \n", actual)
+        fmt.Printf("Expected: %v \n", c.expected)
+        if len(actual) != len(c.expected) {
+            t.Errorf("slices don't match")
+            t.FailNow()
+        }
         for i := range actual {
             word := actual[i]
             expectedWord := c.expected[i]
             // Check each word in the slice
             // if they don't match, use t.Errorf to print an error message
             // and fail the test
+            if word != expectedWord {
+                t.Errorf("Word does not match Expected Word")
+                t.FailNow()
+            }
         }
     }
 }
