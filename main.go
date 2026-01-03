aaaa3dc77488a4bbf0abc2a6a1df12fb0a2562f4
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-01-02 22:02:51 -0500

    Built the bones of the REPL: a loop that parses and cleans an input and prints the first word back to the user

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main()  {
-	fmt.Println("Hello, World!")	
+	prompt := "Pokedex > "
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(prompt)
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	    cleanedInput := cleanInput(scanner.Text())
+	    fmt.Println("Your command was:", cleanedInput[0])
+	    fmt.Print(prompt)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
