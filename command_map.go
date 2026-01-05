74bda18276e43c03be350f467e476be16dd23097
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-01-04 22:53:15 -0500

    Added map command to print locations

---
+++ command_map.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// {"name":"canalave-city-area","url":"https://pokeapi.co/api/v2/location-area/1/"}
+type Location struct {
+    Name string `json:"name"`
+    URL  string `json:"url"`
+}
+
+type LocationResponse struct {
+	Count 		int			`json:"count"`
+	Next		*string		`json:"next"`
+	Previous	*string		`json:"previous"`
+	Results		[]Location	`json:"results"`
+}
+
+// Displays the names of 20 location areas in the Pokemon world.
+//
+// Use:
+// GET https://pokeapi.co/api/v2/location-area/{id or name}/
+//
+// Calling without an id or name will return the first 20 locations, to get
+// the next (or previous) will require using an offset and a limit. Look at the
+// API documentation for more information.
+
+func commandMap() error {
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
+	if err != nil {
+		return err
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	// if res.StatusCode > 299 {
+	// 	log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	// }
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("%s/n", body)
+
+	var locations LocationResponse
+	if err := json.Unmarshal(body, &locations); err != nil {
+		return err
+	}
+
+	for _, location := range locations.Results {
+		fmt.Printf("%s\n", location.Name)
+	}
+
+	return nil
+}
